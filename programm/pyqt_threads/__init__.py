8b821b6e768bdfa71e091648a11b6959c2731d5f
range <alexadush@yandex.ru>
2023-04-18 22:40:51 +0300

    - Separate UI setup from main class to main_window.py - Created package for pyqt windows pyqt_windows - Moved pop_up_windows.py to pyqt_windows package - Added methods create_information_box, create_save_file_dialog, create_yes_no_box to WidgetBuilde class

---
+++ programm/pyqt_threads/__init__.py
@@ -0,0 +1,6 @@
+from .database_thread import DataBaseThread
+from .filter_thread import FilterThread
+from .merge_thread import MergeThread
+from .search_path_thread import SearchPathsThread
+
+__all__ = ["DataBaseThread", "FilterThread", "MergeThread", "SearchPathsThread"]
